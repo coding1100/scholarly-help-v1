ca369b08d7d206261fb79793a46f9ab6badbde2c
coding1100 <coding1343453@gmail.com>
2026-07-28 16:43:12 +0500

    add CSS defering

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import dynamic from "next/dynamic";
 import "./globals.css";
 import { Metadata } from "next";
 import OrganizationSchema from "./components/OrganizationSchema";
+import CssDeferScript from "./components/CssDeferScript";
 
 const ClientScripts = dynamic(() => import("./components/ClientScripts"), {
   ssr: false,
@@ -43,6 +44,9 @@ export default function RootLayout({
         {process.env.NODE_ENV === "production" && (
           <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
         )}
+
+        {/* Defers render-blocking stylesheets on scoped routes (see DEFER_ROUTES) */}
+        <CssDeferScript />
       </head>
       <body suppressHydrationWarning>
         <OrganizationSchema />
